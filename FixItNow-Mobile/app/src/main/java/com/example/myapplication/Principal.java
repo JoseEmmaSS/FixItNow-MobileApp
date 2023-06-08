2e2c5a998685ebd771787d573ffcf0117ebe071d
Arath-ops <arathlugo85@gmail.com>
2023-06-08 00:19:40 -0600

    nav_drawer

--- FixItNow-Mobile/app/src/main/java/com/example/myapplication/Principal.java
+++ FixItNow-Mobile/app/src/main/java/com/example/myapplication/Principal.java
@@ -1,16 +1,25 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 import android.view.MenuItem;
 import androidx.fragment.app.Fragment;
+import androidx.core.view.GravityCompat;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.FragmentManager;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentTransaction;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class Principal extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
+    private DrawerLayout drawerLayout;
 
     private Fragment fragmentHome;
     private Fragment fragmentOrder;
@@ -23,6 +32,21 @@ public class Principal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
 
+        Toolbar toolbar = findViewById(R.id.toolbar); //Ignora linea de errores
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.view);
+        navigationView.setNavigationItemSelectedListener((NavigationView.OnNavigationItemSelectedListener) this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        if (savedInstanceState == null) {
+            switchFragment(fragmentHome);
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+
         // Inicializar fragmentos
         fragmentHome = new HomeFragment();
         fragmentOrder = new OrderFragment();
@@ -34,6 +58,10 @@ public class Principal extends AppCompatActivity {
         // Configurar el BottomNavigationView
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+
+
+
             @Override
             public boolean onNavigationItemSelected(MenuItem item) {
                 switch (item.getItemId()) {
@@ -47,12 +75,23 @@ public class Principal extends AppCompatActivity {
                         switchFragment(fragmentProfile);
                         return true;
                 }
+                drawerLayout.closeDrawer(GravityCompat.START);
                 return false;
             }
         });
 
         // Mostrar el fragmento inicial
         getSupportFragmentManager().beginTransaction().add(R.id.fragmentContainer, fragmentHome).commit();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     private void switchFragment(Fragment fragment) {
